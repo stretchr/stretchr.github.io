4486ea1e0d9df4c43b7dedadb15ee6ff87f57a3d
Mat Ryer <mat@stretchr.com>
2013-10-05 11:28:09 -0600

    more visual tweaks

--- js/application.js
+++ js/application.js
@@ -2,5 +2,7 @@ $(function() {
 	$.typer.options.typerInterval = 3500;
 	$.typer.options.clearDelay = 750;
 	$.typer.options.typeSpeed = 100;
-	$(".typer").typer();
+	$(".typer").typer({
+    "primaryColor": "red"
+  });
 });
