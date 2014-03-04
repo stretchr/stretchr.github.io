401bdb4d53325cd988ec33d7ace1564f191a0b71
Ryan <ryan@mazondo.com>
2014-03-03 21:58:51 -0800

    validate email address before submit

--- js/application.js
+++ js/application.js
@@ -1,20 +1,28 @@
 var stretchr = new Stretchr.Client("stretchr", "demo-invite", "d23e580f5cba40b895d331e09197ace2");
 
+function validateEmail(email) { 
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+} 
 $(function() {
 
 	$(".demo-invite").submit(function(e) {
 		e.preventDefault();
 		var t = $(e.currentTarget),
 			d = t.formalizeData();
-		stretchr.at("invites").create(d, {
-			success: function(response) {
-				t.find(".response").html("<div class='alert alert-danger'>Thanks for your interest - a member of our team will contact you within 24 hours to get you signed up</div>");
-				t.get(0).reset();
-			},
-			error: function(response) {
-				t.find(".response").html("<div class='alert alert-danger'>Uh oh, looks like there was an issue submitting your request, please try again</div>");
-			}
-		})
+		if (!d.email || !validateEmail(d.email)) {
+			t.find(".response").html("<div class='alert alert-danger'>We'll need a valid email address to get in touch.</div>");
+		} else {
+			stretchr.at("invites").create(d, {
+				success: function(response) {
+					t.find(".response").html("<div class='alert alert-info'>Thanks for your interest - a member of our team will contact you within 24 hours to get you signed up</div>");
+					t.get(0).reset();
+				},
+				error: function(response) {
+					t.find(".response").html("<div class='alert alert-danger'>Uh oh, looks like there was an issue submitting your request, please try again</div>");
+				}
+			});
+		}
 	});
 
   	githubStars("stretchr", function(stars) {
